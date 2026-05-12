65126ec6c26954f74b30dbaa6e0dce87d02325a3
Sovan-07 <sovanmondal2003m@gmail.com>
2026-05-12 12:12:32 +0530

    tRPC setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import prisma from "@/lib/db";
+import { caller } from "@/trpc/server";
 import Image from "next/image";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
+  const users = await caller.getUser();
   return (
     <div className="text-red-500">
       {JSON.stringify(users)}
