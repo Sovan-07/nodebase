5d5cccd4848a349910c1b2015ea5e0c71c95d6aa
Sovan-07 <sovanmondal2003m@gmail.com>
2026-05-17 22:13:07 +0530

    workflows CRUD functionality developed

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,29 +1,8 @@
-import { z } from 'zod';
-import { createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
-import { inngest } from '@/inngest/client';
-import {generateText} from 'ai'
-import {google} from '@ai-sdk/google'
+import { createTRPCRouter } from '../init';
+import { workflowsRouter } from '@/features/workflows/server/router';
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async()=>{
-    await inngest.send({
-      name:"execute/ai",
-    })
-    return {success:true , message:"job queued"}
-  }),
-    getWorkflows: protectedProcedure.query(({ctx})=>{
-      return prisma.workflow.findMany();
-    }),
-    createWorkflow : protectedProcedure.mutation(async()=>{
-      await inngest.send({
-        name:"test/hello.world",
-        data:{
-          email:"sovan23@gmail.com",
-        },
-      });
-      return {success:true , message:"job queued"}
-    })
+  workflows: workflowsRouter,
 });
 
 // export type definition of API
